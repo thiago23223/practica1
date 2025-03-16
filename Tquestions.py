58ae02218dd1cd29ebb287af78991d9a8e841b06
thiago23223 <lucenatato2006@gmail.com>
2025-03-16 17:36:45 -0300

    actualizo la primera funcionalidad,donde soluciono el error cuando se ingresa un valor fuera de rango o un caracter q no es un numero

--- Tquestions.py
+++ Tquestions.py
@@ -1,4 +1,5 @@
 import random
+import sys
 # Preguntas para el juego
 questions = [
         "¿Qué función se usa para obtener la longitud de una cadena en Python?",
@@ -32,15 +33,20 @@ for _ in range(3):
                 print(f"{i + 1}. {answer}")
         # El usuario tiene 2 intentos para responder correctamente
         for intento in range(2):
-                user_answer = int(input("Respuesta: ")) - 1
+                user_answer = input("Respuesta: ")
+                if user_answer.isnumeric() and int(user_answer)-1 <= 4 and int(user_answer)-1 >= 0: #verifico si el valor ingresado es un entero y si esta en el rango de respuestas adecuado
                 # Se verifica si la respuesta es correcta
-                if user_answer == correct_answers_index[question_index]:
-                        print("¡Correcto!")
-                        break
+                                user_answer = int(user_answer) - 1 #convierto el numero ingresado a integer
+                                if user_answer == correct_answers_index[question_index]:
+                                        print("¡Correcto!")
+                                        break
+                                else:
+                                # Si el usuario no responde correctamente después de 2 intentos,
+                                # se muestra la respuesta correcta
+                                        print("Incorrecto. La respuesta correcta es:")
+                                        print(answers[question_index][correct_answers_index[question_index]])
                 else:
-                    # Si el usuario no responde correctamente después de 2 intentos,
-                    # se muestra la respuesta correcta
-                        print("Incorrecto. La respuesta correcta es:")
-                        print(answers[question_index][correct_answers_index[question_index]])
+                                print("Respuesta no valida")
+                                sys.exit(1) #finaliza el programa       
         # Se imprime un blanco al final de la pregunta
         print()
