2c8090a980ccf572329044f89f0217880ca30164
thiago23223 <lucenatato2006@gmail.com>
2025-03-20 18:59:19 -0300

    añado funcionalidad para no acceder a las 3 listas usando indices

--- Tquestions.py
+++ Tquestions.py
@@ -26,28 +26,28 @@ correct_answers_index = [1, 2, 0, 3, 1]
 # El usuario deberá contestar 3 preguntas
 puntaje = 0
 # Se selecciona una pregunta aleatoria
-question_index = random.sample(range(len(questions)),3) #me genera una lista con los 3 indices #el primer indice del random.sample me da el rango de la lista, y el segundo cantidad de elementos q se van a elegir
-# Se muestra la pregunta y las respuestas posibles
-for index in question_index: #index es la variable indice por la cual accedo a la lista de preguntas
-        print(questions[index])
-        for i, answer in enumerate(answers[index]):
+question_index = random.sample(list(zip(questions, answers, correct_answers_index)), 3) #me genera una lista con los 3 indices #el primer indice del random.sample me da el rango de la lista, y el segundo cantidad de elementos q se van a elegir #el list(zip()) me genera una lista con los 3 conjuntos de preguntas,respuestas y respuesta correcta elegidos aleatoriamente
+# Se muestra la pregunta y las respuestas posibles 
+for question, answers, correct_answers_index in question_index: #este bucle itera sobre cada variable de la lista con pregunta,respuestas y respuesta.
+        print(question)
+        for i, answer in enumerate(answers):
                 print(f"{i + 1}. {answer}")
         # El usuario tiene 2 intentos para responder correctamente
         for intento in range(2):
                 user_answer = input("Respuesta: ")
                 if user_answer.isnumeric() and int(user_answer) - 1 < 4 and int(user_answer) - 1 >= 0: #verifico si el valor ingresado es un entero y si esta en el rango de respuestas adecuado
                 # Se verifica si la respuesta es correcta
-                                user_answer = int(user_answer) - 1 #convierto el numero ingresado a integer
-                                if user_answer == correct_answers_index[index]:
-                                        print("¡Correcto!")
-                                        puntaje = puntaje + 1
-                                        break
-                                else:
-                                # Si el usuario no responde correctamente después de 2 intentos,
-                                # se muestra la respuesta correcta
-                                        print("Incorrecto. La respuesta correcta es:")
-                                        print(answers[index][correct_answers_index[index]])
-                                        puntaje = puntaje - 0.5
+                        user_answer = int(user_answer) - 1 #convierto el numero ingresado a integer
+                        if user_answer == correct_answers_index:
+                                print("¡Correcto!")
+                                puntaje = puntaje + 1
+                                break
+                        else:
+                        # Si el usuario no responde correctamente después de 2 intentos,
+                        # se muestra la respuesta correcta
+                                print("Incorrecto. La respuesta correcta es:")
+                                print(correct_answers_index + 1)
+                                puntaje = puntaje - 0.5
                 else:
                         print("Respuesta no valida")
                         sys.exit(1) #finaliza el programa       
